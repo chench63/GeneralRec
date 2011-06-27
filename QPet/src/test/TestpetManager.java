7b55e592ad7b71de8b0e2ced000e969d10d0c14a
ppiachen <1333776@tongji.edu.cn>
2011-06-27 12:56:18 +0000

    QPet: update_3

--- QPet/src/test/TestpetManager.java
+++ QPet/src/test/TestpetManager.java
@@ -6,6 +6,11 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import dao.IMatrixDAO;
+import dao.IServicePetDAO;
+import dao.impl.MatrixDAO;
+import dao.impl.ServicePetDAO;
+
 import vo.Itemlib;
 import vo.Itemmatrix;
 import vo.Petlib;
@@ -42,9 +47,10 @@ public class TestpetManager {
 		
 		Usrfrontinfo info = pet.GetInstance(token);
 		Petlib lib= pet.GetDefaultPet(info);
-		System.out.println("\n\n\n************************GetDefaultPet**********");
+		System.out.println("\n************************GetDefaultPet**********");
 		System.out.println("PetId: "+lib.getPetId());
 		System.out.println("SrcUrl: "+lib.getSrcUrl());
+		
 	}
 
 	@Test
@@ -58,16 +64,38 @@ public class TestpetManager {
 		
 		Usrfrontinfo info = pet.GetInstance(token);
 		Servicepet serPet= pet.GetDefaltServicePetItem(info);
-		System.out.println("\n\n\n************************GetDefaultServicePetItem**********");
+		System.out.println("\n************************GetDefaultServicePetItem**********");
 		System.out.println("ServicePetId: "+serPet.getServicePetId());
 		System.out.println("UsrId: "+serPet.getUser().getUsrId());
 		System.out.println("PetId: "+serPet.getPetlib().getPetId());
 		System.out.println("Level: "+serPet.getLevel());
+		
+		assertEquals(serPet.getServicePetId(),info.getServicepet().getServicePetId());
 	}
 
 	@Test
 	public void testCloseServicePetItem() {
-		//fail("Not yet implemented");
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+		petManager pet = (petManager) ctx.getBean("petManager");
+		
+		String token="LXUGQ2YXP NCKVSU7G5QIMKI LON2DT P02WAXI4PPCLT70SLWSCF6X078Y68LENMRR0H57KBIVO5F6PFO3ORCOG3DYLGB8Y37BYY18RD79XUTTBV" +
+		"MXGTJV0F5O68E5TPY3VN4FJI6IJKR0PV5774B29M952BTFKQ9JRR 362HNO6UAE5FUDKFC714FCPOWVA31IRH2F9US" +
+		"BWQRB65SD9QB7OSM0S90XA030EJRC1O0E5C4S8S1HK853GYUX32L";
+		
+		Usrfrontinfo info = pet.GetInstance(token);
+		Servicepet servicepet= pet.GetDefaltServicePetItem(info);
+		int level = 10;
+		int exp = 100;
+		servicepet.setExp(exp);
+		servicepet.setLevel(level);
+		pet.CloseServicePetItem(servicepet);
+		
+		System.out.println("\n************************CloseServiceItem**********");
+		
+		IServicePetDAO dao = (ServicePetDAO) ctx.getBean("ServicePetDAO");;
+		Servicepet servicepetCheck= dao.findById(servicepet.getServicePetId());
+		assertEquals(servicepet.getExp(),servicepetCheck.getExp());
+		assertEquals(servicepet.getLevel(),servicepetCheck.getLevel());
 	}
 
 	@Test
@@ -81,7 +109,7 @@ public class TestpetManager {
 		
 		Usrfrontinfo info = pet.GetInstance(token);
 		Itemmatrix item=pet.GetRatingItem(info);
-		System.out.println("\n\n\n************************GetRatingItem**********");
+		System.out.println("\n************************GetRatingItem**********");
 		System.out.println("ItemId: "+item.getItemId());
 		System.out.println("ImgUrl: "+item.getImgUrl());
 		System.out.println("WebUrl: "+item.getWebUrl());
@@ -101,7 +129,8 @@ public class TestpetManager {
 		Usrfrontinfo info = pet.GetInstance(token);
 		int score = 10;
 		pet.CloseRatingItem(info, score);
-		System.out.println("\n\n\n************************CloseRatingItem**********");
+		System.out.println("\n************************CloseRatingItem**********");
+		
 	}
 
 	@Test
@@ -125,7 +154,7 @@ public class TestpetManager {
 		item.setImgUrl("Test");
 		item.setWebUrl("Test");
 		pet.AppendInterestingItem(item, info);
-		System.out.println("\n\n\n************************AppendInterestingItem**********");
+		System.out.println("\n************************AppendInterestingItem**********");
 	}
 
 }
