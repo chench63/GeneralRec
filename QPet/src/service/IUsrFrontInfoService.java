8fdd3b686eaee0e34a4aac7acda6bd74bed4b54e
ppiachen <1333776@tongji.edu.cn>
2011-07-09 15:10:12 +0000

    QPet:  update_4

--- QPet/src/service/IUsrFrontInfoService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import vo.User;
-import vo.Usrfrontinfo;
- 
-public interface IUsrFrontInfoService {
-	public Usrfrontinfo GetInstanceByUsr(User instance);
-	public void saveOrUpdate(Usrfrontinfo instance);
-}
