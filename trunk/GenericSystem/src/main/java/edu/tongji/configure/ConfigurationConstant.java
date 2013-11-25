b9fca91a9715744b885e6588a637a479e5184d78
ppiachen@gmail.com <ppiachen@gmail.com@9f54f1a8-f05c-94ce-c301-9acde9563bef>
2013-11-25 03:36:43 +0000

    重构实验部分

--- trunk/GenericSystem/src/main/java/edu/tongji/configure/ConfigurationConstant.java
+++ trunk/GenericSystem/src/main/java/edu/tongji/configure/ConfigurationConstant.java
@@ -10,8 +10,6 @@ package edu.tongji.configure;
  * <p> 
  *  任务容量：<br>
  *      主任务参数[I, TASK_SIZE)<br>
- *
- *      
  * </p>
  *  
  * @author chenkh
@@ -20,32 +18,123 @@ package edu.tongji.configure;
 public final class ConfigurationConstant {
 
     /**  最大缓存容量 $Cache*/
-    public final static int    MAX_CACHE_SIZE   = 10000;
+    public static int    MAX_CACHE_SIZE   = 10000;
 
     /**  单任务最大容量 $Cache*/
-    public final static int    SINGLE_TASK_SIZE = 1000;
+    public static int    SINGLE_TASK_SIZE = 1000;
 
     /**  主任务参数最大值 $Cache*/
-    public final static int    TASK_SIZE        = 885;
+    public static int    TASK_SIZE        = 885;
 
     /**  子任务容量 $CacheStopWatch : 使用分组，必须保证，分组数 >= 2*/
-    public final static int    SUB_TASK_SIZE    = 177;
+    public static int    SUB_TASK_SIZE    = 177;
 
     /**  主任务参数i, 分配任务式使用 $CacheTask*/
-    public final static int    I                = 2;
+    public static int    I                = 2;
 
     /**  协任务参数j, 分配任务式使用 $CacheTask*/
-    public final static int    J                = 1;
+    public static int    J                = 1;
 
     /** DB捞取数据：开始movieId号*/
-    public final static int    movieStart       = 1;
+    public static int    movieStart       = 1;
 
     /** DB捞取数据：结束movieId号*/
-    public final static int    movieEnd         = 885;
+    public static int    movieEnd         = 885;
 
     /** DB捞取数据：开始时间轴*/
-    public final static String scrachTimeLine   = "2000-12-31 00:00:00";
+    public static String scrachTimeLine   = "2000-12-31 00:00:00";
 
     /** DB捞取数据：截止时间轴*/
-    public final static String endTimeLine      = "2005-12-31 00:00:00";
+    public static String endTimeLine      = "2005-12-31 00:00:00";
+
+    /**
+     * Setter method for property <tt>mAX_CACHE_SIZE</tt>.
+     * 
+     * @param MAX_CACHE_SIZE value to be assigned to property mAX_CACHE_SIZE
+     */
+    public static void setMAX_CACHE_SIZE(int mAX_CACHE_SIZE) {
+        MAX_CACHE_SIZE = mAX_CACHE_SIZE;
+    }
+
+    /**
+     * Setter method for property <tt>sINGLE_TASK_SIZE</tt>.
+     * 
+     * @param SINGLE_TASK_SIZE value to be assigned to property sINGLE_TASK_SIZE
+     */
+    public static void setSINGLE_TASK_SIZE(int sINGLE_TASK_SIZE) {
+        SINGLE_TASK_SIZE = sINGLE_TASK_SIZE;
+    }
+
+    /**
+     * Setter method for property <tt>tASK_SIZE</tt>.
+     * 
+     * @param TASK_SIZE value to be assigned to property tASK_SIZE
+     */
+    public static void setTASK_SIZE(int tASK_SIZE) {
+        TASK_SIZE = tASK_SIZE;
+    }
+
+    /**
+     * Setter method for property <tt>sUB_TASK_SIZE</tt>.
+     * 
+     * @param SUB_TASK_SIZE value to be assigned to property sUB_TASK_SIZE
+     */
+    public static void setSUB_TASK_SIZE(int sUB_TASK_SIZE) {
+        SUB_TASK_SIZE = sUB_TASK_SIZE;
+    }
+
+    /**
+     * Setter method for property <tt>i</tt>.
+     * 
+     * @param I value to be assigned to property i
+     */
+    public static void setI(int i) {
+        I = i;
+    }
+
+    /**
+     * Setter method for property <tt>j</tt>.
+     * 
+     * @param J value to be assigned to property j
+     */
+    public static void setJ(int j) {
+        J = j;
+    }
+
+    /**
+     * Setter method for property <tt>movieStart</tt>.
+     * 
+     * @param movieStart value to be assigned to property movieStart
+     */
+    public static void setMovieStart(int movieStart) {
+        ConfigurationConstant.movieStart = movieStart;
+    }
+
+    /**
+     * Setter method for property <tt>movieEnd</tt>.
+     * 
+     * @param movieEnd value to be assigned to property movieEnd
+     */
+    public static void setMovieEnd(int movieEnd) {
+        ConfigurationConstant.movieEnd = movieEnd;
+    }
+
+    /**
+     * Setter method for property <tt>scrachTimeLine</tt>.
+     * 
+     * @param scrachTimeLine value to be assigned to property scrachTimeLine
+     */
+    public static void setScrachTimeLine(String scrachTimeLine) {
+        ConfigurationConstant.scrachTimeLine = scrachTimeLine;
+    }
+
+    /**
+     * Setter method for property <tt>endTimeLine</tt>.
+     * 
+     * @param endTimeLine value to be assigned to property endTimeLine
+     */
+    public static void setEndTimeLine(String endTimeLine) {
+        ConfigurationConstant.endTimeLine = endTimeLine;
+    }
+    
 }
--- trunk/GenericSystem/src/main/java/edu/tongji/configure/TestCaseConfigurationConstant.java
+++ trunk/GenericSystem/src/main/java/edu/tongji/configure/TestCaseConfigurationConstant.java
@@ -12,39 +12,129 @@ package edu.tongji.configure;
 public final class TestCaseConfigurationConstant {
 
     /** 测试集大小 */
-    public final static int     TESTCASE_SIZE         = 200;
+    public static int          TESTCASE_SIZE         = 200;
 
     /** 测试集左区间 */
-    public final static int     LEFT_SIDE             = 1;
+    public static int          LEFT_SIDE             = 1;
 
     /** 测试集右区间 */
-    public final static int     RIGHT_SIDE            = 2649429;
+    public static int          RIGHT_SIDE            = 2649429;
 
     /** 测试集是否随机产生*/
-    public final static boolean NEED_RANDOM_TESTCASE  = false;
+    public static boolean      NEED_RANDOM_TESTCASE  = false;
 
     /** 测试集是否使用扰动效果*/
-    public final static boolean IS_PERTURBATION       = false;
+    public static boolean      IS_PERTURBATION       = false;
 
     /** 绕送是否使用高斯噪声 */
-    public final static boolean IS_NORMAL             = false;
+    public static boolean      IS_NORMAL             = false;
 
     /** 测试集，高斯分布扰动概率范围 0.67-1.96*/
-    public final static double  PERTURBATION_DOMAIN   = 0.67;
+    public static double       PERTURBATION_DOMAIN   = 0.67;
 
     /** 自定义测试集合*/
-    public final static String  TEST_CASE             = "700638, 65666, 2456884, 1877798, 33079, 1703932, 809671, 482597, 1771703"
-                                                        + ",344444, 1025476, 1124058, 1561358, 1176808, 241923, 216623, 1402512, 722332, 2497412, 1341871, 283675, "
-                                                        + "1446922, 2453475, 328595, 1778888, 427770, 2381092, 296253, 192650, 656431, 472804, "
-                                                        + "165393, 2560090, 159639, 2002276, 1122405, 999027, 1735892, "
-                                                        + "1948535, 1645137, 2553999, 2201630, 2229348, 2059528";
+    public static String       TEST_CASE             = "700638, 65666, 2456884, 1877798, 33079, 1703932, 809671, 482597, 1771703"
+                                                       + ",344444, 1025476, 1124058, 1561358, 1176808, 241923, 216623, 1402512, 722332, 2497412, 1341871, 283675, "
+                                                       + "1446922, 2453475, 328595, 1778888, 427770, 2381092, 296253, 192650, 656431, 472804, "
+                                                       + "165393, 2560090, 159639, 2002276, 1122405, 999027, 1735892, "
+                                                       + "1948535, 1645137, 2553999, 2201630, 2229348, 2059528";
     /** 测试集使用的训练相似度*/
-    public final static String  TEST_CASE_SIMULARITY  = "";
+    public static String       TEST_CASE_SIMULARITY  = "";
 
     /** 分隔符正则表达式 */
-    public static String        SAPERATOR_EXPRESSION  = "\\,";
+    public final static String SAPERATOR_EXPRESSION  = "\\,";
 
     /** 載入相似度范围的右区间   *999999999*/
-    public final static int     SIMILARITY_RIGHT_SIDE = 885;
+    public static int          SIMILARITY_RIGHT_SIDE = 885;
+
+    /**
+     * Setter method for property <tt>tESTCASE_SIZE</tt>.
+     * 
+     * @param TESTCASE_SIZE value to be assigned to property tESTCASE_SIZE
+     */
+    public static void setTESTCASE_SIZE(int tESTCASE_SIZE) {
+        TESTCASE_SIZE = tESTCASE_SIZE;
+    }
+
+    /**
+     * Setter method for property <tt>lEFT_SIDE</tt>.
+     * 
+     * @param LEFT_SIDE value to be assigned to property lEFT_SIDE
+     */
+    public static void setLEFT_SIDE(int lEFT_SIDE) {
+        LEFT_SIDE = lEFT_SIDE;
+    }
+
+    /**
+     * Setter method for property <tt>rIGHT_SIDE</tt>.
+     * 
+     * @param RIGHT_SIDE value to be assigned to property rIGHT_SIDE
+     */
+    public static void setRIGHT_SIDE(int rIGHT_SIDE) {
+        RIGHT_SIDE = rIGHT_SIDE;
+    }
+
+    /**
+     * Setter method for property <tt>nEED_RANDOM_TESTCASE</tt>.
+     * 
+     * @param NEED_RANDOM_TESTCASE value to be assigned to property nEED_RANDOM_TESTCASE
+     */
+    public static void setNEED_RANDOM_TESTCASE(boolean nEED_RANDOM_TESTCASE) {
+        NEED_RANDOM_TESTCASE = nEED_RANDOM_TESTCASE;
+    }
+
+    /**
+     * Setter method for property <tt>iS_PERTURBATION</tt>.
+     * 
+     * @param IS_PERTURBATION value to be assigned to property iS_PERTURBATION
+     */
+    public static void setIS_PERTURBATION(boolean iS_PERTURBATION) {
+        IS_PERTURBATION = iS_PERTURBATION;
+    }
+
+    /**
+     * Setter method for property <tt>iS_NORMAL</tt>.
+     * 
+     * @param IS_NORMAL value to be assigned to property iS_NORMAL
+     */
+    public static void setIS_NORMAL(boolean iS_NORMAL) {
+        IS_NORMAL = iS_NORMAL;
+    }
+
+    /**
+     * Setter method for property <tt>pERTURBATION_DOMAIN</tt>.
+     * 
+     * @param PERTURBATION_DOMAIN value to be assigned to property pERTURBATION_DOMAIN
+     */
+    public void setPERTURBATION_DOMAIN(double pERTURBATION_DOMAIN) {
+        PERTURBATION_DOMAIN = pERTURBATION_DOMAIN;
+    }
+
+    /**
+     * Setter method for property <tt>tEST_CASE</tt>.
+     * 
+     * @param TEST_CASE value to be assigned to property tEST_CASE
+     */
+    public static void setTEST_CASE(String tEST_CASE) {
+        TEST_CASE = tEST_CASE;
+    }
+
+    /**
+     * Setter method for property <tt>tEST_CASE_SIMULARITY</tt>.
+     * 
+     * @param TEST_CASE_SIMULARITY value to be assigned to property tEST_CASE_SIMULARITY
+     */
+    public static void setTEST_CASE_SIMULARITY(String tEST_CASE_SIMULARITY) {
+        TEST_CASE_SIMULARITY = tEST_CASE_SIMULARITY;
+    }
+
+    /**
+     * Setter method for property <tt>sIMILARITY_RIGHT_SIDE</tt>.
+     * 
+     * @param SIMILARITY_RIGHT_SIDE value to be assigned to property sIMILARITY_RIGHT_SIDE
+     */
+    public static void setSIMILARITY_RIGHT_SIDE(int sIMILARITY_RIGHT_SIDE) {
+        SIMILARITY_RIGHT_SIDE = sIMILARITY_RIGHT_SIDE;
+    }
 
 }
