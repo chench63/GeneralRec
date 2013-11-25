3a4589b1c24a851becb7a9dff1c16f129744b58e
ppiachen@gmail.com <ppiachen@gmail.com@9f54f1a8-f05c-94ce-c301-9acde9563bef>
2013-11-25 03:03:17 +0000

    提炼实验模式

--- trunk/GenericSystem/src/test/java/edu/tongji/experiment/NetflixAsynchronizedSMPExper.java
+++ trunk/GenericSystem/src/test/java/edu/tongji/experiment/NetflixAsynchronizedSMPExper.java
@@ -2,7 +2,7 @@
  * Tongji Edu.
  * Copyright (c) 2004-2013 All Rights Reserved.
  */
-package edu.tongji.thread.main;
+package edu.tongji.experiment;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,12 +10,11 @@ import edu.tongji.engine.Engine;
 import edu.tongji.util.ExceptionUtil;
 
 /**
- * 计算Netflix数据集，用户Rating丢失比例的主线程程序。
  * 
  * @author chenkh
- * @version $Id: NetflixRatingMainThread.java, v 0.1 2013-10-8 下午5:53:05 chenkh Exp $
+ * @version $Id: NetflixSimlarityPerformanceMainThread.java, v 0.1 2013-10-15 上午10:23:03 chenkh Exp $
  */
-public final class NetflixRatingMainThread {
+public final class NetflixAsynchronizedSMPExper {
 
     /**
      * 
@@ -25,8 +24,8 @@ public final class NetflixRatingMainThread {
         ClassPathXmlApplicationContext ctx = null;
         try {
             ctx = new ClassPathXmlApplicationContext(
-                "META-INF/spring/application-context-netflix-statistic.xml");
-            Engine engine = (Engine) ctx.getBean("engine");
+                "experiment/recommendation/netflix/asynchronized-secure-mulitiparty.xml");
+            Engine engine = (Engine) ctx.getBean("engineForGeneratingRecommendations");
             engine.excute();
         } catch (Exception e) {
             ExceptionUtil.caught(e, "edu.tongji.engine.TestEngine 测试用例发生错误");
