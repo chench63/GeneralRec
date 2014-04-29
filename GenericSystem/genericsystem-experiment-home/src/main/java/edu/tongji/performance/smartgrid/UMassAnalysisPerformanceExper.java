a51b61f287320aca4836ee2fa4c063d805361655
ppiachen <1333776@tongji.edu.cn>
2014-04-29 12:26:32 +0000

    构建maven多模块结构

---
+++ GenericSystem/genericsystem-experiment-home/src/main/java/edu/tongji/performance/smartgrid/UMassAnalysisPerformanceExper.java
@@ -0,0 +1,39 @@
+/**
+ * Tongji Edu.
+ * Copyright (c) 2004-2014 All Rights Reserved.
+ */
+package edu.tongji.performance.smartgrid;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import edu.tongji.engine.smartgrid.SmartGridEngine;
+import edu.tongji.experiment.smartgrid.UMassOneTimePadExper;
+import edu.tongji.util.ExceptionUtil;
+
+/**
+ * 
+ * @author chench
+ * @version $Id: UMassAnalysisPerformanceExper.java, v 0.1 2014-3-18 上午10:14:27 chench Exp $
+ */
+public class UMassAnalysisPerformanceExper {
+
+    @Test
+    public void test() {
+        ClassPathXmlApplicationContext ctx = null;
+        try {
+            ctx = new ClassPathXmlApplicationContext(
+                "performance/smartgrid/umass/umass-analysis-performance.xml");
+            SmartGridEngine engine = (SmartGridEngine) ctx.getBean("analysisEngine");
+            engine.excute();
+
+        } catch (Exception e) {
+            ExceptionUtil.caught(e, UMassOneTimePadExper.class + " 发生错误");
+        } finally {
+            if (ctx != null) {
+                ctx.close();
+            }
+        }
+    }
+
+}
