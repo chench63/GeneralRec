bdbfb64c9d9c66bada03009941cf098fe8b2ff60
ppiachen <1333776@tongji.edu.cn>
2014-05-01 03:18:19 +0000

    维护代码，修草坪

--- GenericSystem/genericsystem-core-service/src/main/java/edu/tongji/thread/NetflixEvaPredctRecorder.java
+++ GenericSystem/genericsystem-core-service/src/main/java/edu/tongji/thread/NetflixEvaPredctRecorder.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.apache.log4j.Logger;
 
 import edu.tongji.cache.CacheTask;
@@ -33,144 +32,223 @@ import edu.tongji.vo.RatingVO;
  */
 public class NetflixEvaPredctRecorder extends Thread {
 
-	/** logger */
-	private final static Logger logger = Logger
-			.getLogger(LoggerDefineConstant.SERVICE_NORMAL);
+    /** logger */
+    private final static Logger                  logger      = Logger
+                                                                 .getLogger(LoggerDefineConstant.SERVICE_NORMAL);
 
-	/** 预测器 */
-	private Predictor predictor;
+    /** 预测器 */
+    private Predictor                            predictor;
 
-	/** 全局统计器 */
-	protected static final DescriptiveStatistics STAT = new DescriptiveStatistics();
+    /** 全局统计器 */
+    protected static final StatisticsGlobalInner STAT        = new StatisticsGlobalInner();
 
-	/**
-	 * @see java.lang.Thread#run()
-	 */
-	@Override
-	public void run() {
+    /** 全局统计器 */
+    protected static final StatisticsGlobalInner SQUARE_STAT = new StatisticsGlobalInner();
 
-		CacheTask task = null;
-		StatisticsInner statGlobal = new StatisticsInner();
-		while ((task = GeneralCache.task()) != null) {
-			// 1. 读取文件
-			File file = (File) task.get(CacheTask.FILE);
-			String[] contents = FileUtil.readLines(file.getAbsolutePath());
-			List<RatingVO> prsnHistry = new ArrayList<RatingVO>(contents.length);
-			for (int i = 1; i < contents.length; i++) {
-				ParserTemplate template = new ParserTemplate();
-				template.setTemplate(contents[i]);
+    /**
+     * @see java.lang.Thread#run()
+     */
+    @Override
+    public void run() {
 
-				prsnHistry
-						.add((RatingVO) TemplateType.NETFLIX_RATINGVO_TEMPLATE
-								.parser(template));
-			}
+        CacheTask task = null;
+        StatisticsInner statGlobal = new StatisticsInner();
+        StatisticsInner statSquareGlobal = new StatisticsInner();
+        while ((task = GeneralCache.task()) != null) {
+            // 1. 读取文件
+            File file = (File) task.get(CacheTask.FILE);
+            String[] contents = FileUtil.readLines(file.getAbsolutePath());
+            List<RatingVO> prsnHistry = new ArrayList<RatingVO>(contents.length);
+            for (int i = 1; i < contents.length; i++) {
+                ParserTemplate template = new ParserTemplate();
+                template.setTemplate(contents[i]);
 
-			// 2. 生产预测评分
-			PredictorHolder prdctHlder = new PredictorHolder();
-			prdctHlder.put(PredictorHolder.PERSON_RATING_HISTRY, prsnHistry);
-			predictor.predict(prdctHlder);
+                prsnHistry.add((RatingVO) TemplateType.NETFLIX_RATINGVO_TEMPLATE.parser(template));
+            }
 
-			// 3. 载入日志
-			StatisticsInner statSingle = new StatisticsInner();
-			for (RatingVO rating : prsnHistry) {
-				// GeneralPredictor 预测值存于RatingCmp中
-				statGlobal.addValue(Math.abs(rating.getRatingReal()
-						- rating.getRatingCmp()));
-				statSingle.addValue(Math.abs(rating.getRatingReal()
-						- rating.getRatingCmp()));
-			}
-			LoggerUtil.info(
-					logger,
-					new StringBuilder("File: ").append(file.getName())
-							.append('\t').append(statSingle));
+            // 2. 生产预测评分
+            PredictorHolder prdctHlder = new PredictorHolder();
+            prdctHlder.put(PredictorHolder.PERSON_RATING_HISTRY, prsnHistry);
+            predictor.predict(prdctHlder);
 
-			// 4. 删除任务文件
-			file.delete();
-		}
+            // 3. 载入日志
+            StatisticsInner statSingle = new StatisticsInner();
+            for (RatingVO rating : prsnHistry) {
+                // GeneralPredictor 预测值存于RatingCmp中
+                statGlobal.addValue(Math.abs(rating.getRatingReal() - rating.getRatingCmp()));
+                statSquareGlobal.addValue(Math.pow(rating.getRatingReal() - rating.getRatingCmp(),
+                    2.0));
+                statSingle.addValue(Math.abs(rating.getRatingReal() - rating.getRatingCmp()));
+            }
+            LoggerUtil.info(logger, new StringBuilder("File: ").append(file.getName()).append('\t')
+                .append(statSingle));
 
-		// 4. 输出日志
-		completeAndReport(statGlobal);
+        }
 
-	}
+        // 4. 输出日志
+        completeAndReport(statGlobal, statSquareGlobal);
 
-	/**
-	 * 结束并报告线程计算结果
-	 * 
-	 * @param statGlobal
-	 */
-	protected synchronized void completeAndReport(StatisticsInner statGlobal) {
-		STAT.addValue(statGlobal.getAverage());
+    }
 
-		if (STAT.getN() == ConfigurationConstant.THREAD_SIZE) {
-			LoggerUtil.info(
-					logger,
-					(new StringBuilder(FileUtil.BREAK_LINE))
-							.append("Attention!").append(FileUtil.BREAK_LINE)
-							.append("S: ").append(STAT.getSum())
-							.append(FileUtil.BREAK_LINE).append("N: ")
-							.append(STAT.getN()));
-		}
-	}
+    /**
+     * 结束并报告线程计算结果
+     * 
+     * @param statGlobal
+     */
+    protected synchronized void completeAndReport(StatisticsInner statGlobal,
+                                                  StatisticsInner statSquareGlobal) {
+        //输出个体日志
+        LoggerUtil.info(logger, (new StringBuilder("Task Completes: ")).append(statGlobal));
+        STAT.addValue(statGlobal.sum, statGlobal.N);
+        SQUARE_STAT.addValue(statSquareGlobal.sum, statSquareGlobal.N);
 
-	/**
-	 * 简化统计内部类，节约内存
-	 * 
-	 * @author chench
-	 * @version $Id: NetflixEvaPredctRecorder.java, v 0.1 24 Apr 2014 19:48:23
-	 *          chench Exp $
-	 */
-	protected class StatisticsInner {
+        if (STAT.getN() == ConfigurationConstant.THREAD_SIZE
+            && SQUARE_STAT.getN() == ConfigurationConstant.THREAD_SIZE) {
+            //RMSE = Sqrt[ E(X*X) ]
+            double RMSE = Math.sqrt(SQUARE_STAT.getAverage());
+            //MAE = E(X)
+            double MAE = STAT.getAverage();
+            //SD = SD(X) = Sqrt[ E(X*X) - E(X)*E(X)  ]
+            double SD = Math.sqrt(SQUARE_STAT.getAverage() - Math.pow(MAE, 2.0));
+            //输出日志
+            LoggerUtil.info(
+                logger,
+                (new StringBuilder(FileUtil.BREAK_LINE)).append("Attention!")
+                    .append(FileUtil.BREAK_LINE).append("RMSE: ").append(RMSE)
+                    .append(FileUtil.BREAK_LINE).append("Mean: ").append(MAE)
+                    .append(FileUtil.BREAK_LINE).append("SD: ").append(SD));
+        }
+    }
 
-		/** 和 */
-		private float sum = 0.0f;
+    /**
+     * 
+     * 
+     * @author chench
+     * @version $Id: NetflixEvaPredctRecorder.java, v 0.1 2014-4-30 下午7:59:28 chench Exp $
+     */
+    protected static class StatisticsGlobalInner {
 
-		/** 累计个数 */
-		private int N = 0;
+        /** 和 */
+        private double sum = 0.0d;
 
-		/**
-		 * 添加值
-		 * 
-		 * @param val
-		 */
-		public void addValue(float val) {
-			sum += val;
-			N++;
-		}
+        /** 求和样本个数*/
+        private int    num = 0;
 
-		/**
-		 * 返回均值
-		 * 
-		 * @return
-		 */
-		public float getAverage() {
-			if (N == 0) {
-				return Float.NaN;
-			}
+        /** 累计使用次数 */
+        private int    N   = 0;
 
-			return sum / N;
-		}
+        /**
+         * 添加值
+         * 
+         * @param val
+         * @param n
+         */
+        public void addValue(double val, double n) {
+            if (Double.isNaN(val)) {
+                return;
+            }
 
-		/**
-		 * @see java.lang.Object#toString()
-		 */
-		@Override
-		public String toString() {
-			return (new StringBuilder("Sum: "))
-					.append(StringUtil.alignLeft(String.format("%.3f", sum), 12))
-					.append(" N: ")
-					.append(StringUtil.alignLeft(String.format("%d", N), 12))
-					.append(" Avg: ").append(this.getAverage()).toString();
-		}
-	}
+            this.sum += val;
+            this.num += n;
+            N++;
+        }
 
-	/**
-	 * Setter method for property <tt>predictor</tt>.
-	 * 
-	 * @param predictor
-	 *            value to be assigned to property predictor
-	 */
-	public void setPredictor(Predictor predictor) {
-		this.predictor = predictor;
-	}
+        /**
+         * 返回均值
+         * 
+         * @return
+         */
+        public double getAverage() {
+            if (N == 0 | num == 0) {
+                return Double.NaN;
+            }
+
+            return sum / num;
+        }
+
+        /**
+         * Getter method for property <tt>n</tt>.
+         * 
+         * @return property value of N
+         */
+        public int getN() {
+            return N;
+        }
+
+        /** 
+         * @see java.lang.Object#toString()
+         */
+        @Override
+        public String toString() {
+            return (new StringBuilder("S: ")).append(sum).append(FileUtil.BREAK_LINE).append("N: ")
+                .append(N).append(FileUtil.BREAK_LINE).append("Mean: ").append(this.getAverage())
+                .toString();
+        }
+
+    }
+
+    /**
+     * 简化统计内部类，节约内存
+     * 
+     * @author chench
+     * @version $Id: NetflixEvaPredctRecorder.java, v 0.1 24 Apr 2014 19:48:23
+     *          chench Exp $
+     */
+    protected class StatisticsInner {
+
+        /** 和 */
+        private double sum = 0.0d;
+
+        /** 累计个数 */
+        private int    N   = 0;
+
+        /**
+         * 添加值
+         * 
+         * @param val
+         */
+        public void addValue(double val) {
+            if (Double.isNaN(val)) {
+                return;
+            }
+
+            sum += val;
+            N++;
+        }
+
+        /**
+         * 返回均值
+         * 
+         * @return
+         */
+        public double getAverage() {
+            if (N == 0) {
+                return Double.NaN;
+            }
+
+            return sum / N;
+        }
+
+        /**
+         * @see java.lang.Object#toString()
+         */
+        @Override
+        public String toString() {
+            return (new StringBuilder("Sum: "))
+                .append(StringUtil.alignLeft(String.format("%.3f", sum), 12)).append(" N: ")
+                .append(StringUtil.alignLeft(String.format("%d", N), 12)).append(" Mean: ")
+                .append(this.getAverage()).toString();
+        }
+    }
+
+    /**
+     * Setter method for property <tt>predictor</tt>.
+     * 
+     * @param predictor
+     *            value to be assigned to property predictor
+     */
+    public void setPredictor(Predictor predictor) {
+        this.predictor = predictor;
+    }
 
 }
