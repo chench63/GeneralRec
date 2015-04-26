edd93567f23136ae63f02c6d8e4cfa58575fb1a3
ppiachen@gmail.com <ppiachen@gmail.com@9f54f1a8-f05c-94ce-c301-9acde9563bef>
2015-04-26 04:16:46 +0000

    Rewrite PreaLLORMA codes

--- trunk/PreaLLORMA/src/main/java/prea/util/FileUtil.java
+++ trunk/PreaLLORMA/src/main/java/prea/util/FileUtil.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import org.apache.commons.io.IOUtils;
+
 /**
  * 简单文件处理工具，读取文件中所有行。
  * 
@@ -43,6 +45,84 @@ public final class FileUtil {
     }
 
     /**
+     * 简单读取文件，
+     * 返回文件所有行，且去掉空格.
+     * 
+     * @param path   文件路径
+     * @return
+     */
+    public static String readLinesAsStream(String path) {
+        File file = new File(path);
+
+        //读取并解析数据
+        if (!file.isFile() | !file.exists()) {
+            ExceptionUtil.caught(new FileNotFoundException("File Not Found"), "读取文件发生异常，校验文件路径: "
+                                                                              + path);
+            return null;
+        }
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String context = null;
+            while ((context = reader.readLine()) != null) {
+                stringBuilder.append(StringUtil.trim(context)).append(StringUtil.BREAK_LINE);
+            }
+
+            return stringBuilder.toString();
+        } catch (FileNotFoundException e) {
+            ExceptionUtil.caught(e, "无法找到对应的加载文件: " + path);
+        } catch (IOException e) {
+            ExceptionUtil.caught(e, "读取文件发生异常，校验文件格式");
+        } finally {
+            IOUtils.closeQuietly(reader);
+        }
+
+        //出现异常，返回null
+        return null;
+    }
+
+    /**
+     * 简单读取文件，
+     * 返回文件所有行，且去掉空格.
+     * 
+     * @param path   文件路径
+     * @return
+     */
+    public static String[] readLines(String path) {
+        File file = new File(path);
+
+        //读取并解析数据
+        if (!file.isFile() | !file.exists()) {
+            ExceptionUtil.caught(new FileNotFoundException("File Not Found"), "读取文件发生异常，校验文件路径: "
+                                                                              + path);
+            return null;
+        }
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+
+            List<String> lines = new ArrayList<String>();
+            String context = null;
+            while ((context = reader.readLine()) != null) {
+                lines.add(StringUtil.trim(context));
+            }
+
+            return lines.toArray(new String[lines.size()]);
+        } catch (FileNotFoundException e) {
+            ExceptionUtil.caught(e, "无法找到对应的加载文件: " + path);
+        } catch (IOException e) {
+            ExceptionUtil.caught(e, "读取文件发生异常，校验文件格式");
+        } finally {
+            IOUtils.closeQuietly(reader);
+        }
+
+        //出现异常，返回null
+        return null;
+    }
+
+    /**
      * 根据通配符规则，读取多个文件
      * 
      * @param path
@@ -59,8 +139,8 @@ public final class FileUtil {
         String regexValue = path.substring(index + 1);
         File dir = new File(dirValue);
         if (!dir.isDirectory() | StringUtil.isBlank(regexValue)) {
-            //            ExceptionUtil.caught(new FileNotFoundException("File Not Found"), "目录不存在，校验文件路径: "
-            //                                                                              + path);
+            ExceptionUtil.caught(new FileNotFoundException("File Not Found"), "目录不存在，校验文件路径: "
+                                                                              + path);
             return null;
         }
 
@@ -80,11 +160,11 @@ public final class FileUtil {
                     }
 
                 } catch (FileNotFoundException e) {
-                    //                    ExceptionUtil.caught(e, "无法找到对应的加载文件: " + path);
+                    ExceptionUtil.caught(e, "无法找到对应的加载文件: " + path);
                 } catch (IOException e) {
-                    //                    ExceptionUtil.caught(e, "读取文件发生异常，校验文件格式");
+                    ExceptionUtil.caught(e, "读取文件发生异常，校验文件格式");
                 } finally {
-                    //                    IOUtils.closeQuietly(reader);
+                    IOUtils.closeQuietly(reader);
                 }
             }
         }
@@ -109,8 +189,8 @@ public final class FileUtil {
         String regexValue = path.substring(index + 1);
         File dir = new File(dirValue);
         if (!dir.isDirectory() | StringUtil.isBlank(regexValue)) {
-            //            ExceptionUtil.caught(new FileNotFoundException("File Not Found"), "目录不存在，校验文件路径: "
-            //                                                                              + path);
+            ExceptionUtil.caught(new FileNotFoundException("File Not Found"), "目录不存在，校验文件路径: "
+                                                                              + path);
             return null;
         }
 
@@ -149,9 +229,35 @@ public final class FileUtil {
             writer = new BufferedWriter(new FileWriter(file));
             writer.write(context);
         } catch (IOException e) {
-            //            ExceptionUtil.caught(e, "写文件发生异常，校验文件格式");
+            ExceptionUtil.caught(e, "写文件发生异常，校验文件格式");
         } finally {
-            //            IOUtils.closeQuietly(writer);
+            IOUtils.closeQuietly(writer);
+        }
+    }
+
+    /**
+     * Append the content into the given file
+     * 
+     * @param file      the file to write
+     * @param context   the content to write
+     */
+    public static void writeAsAppendWithDirCheck(String file, String context) {
+        FileWriter writer = null;
+        try {
+            //make sure parent directory exist
+            File f = new File(file);
+            File dir = f.getParentFile();
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            //write information to disk
+            writer = new FileWriter(file, true);
+            writer.append(context);
+        } catch (IOException e) {
+            ExceptionUtil.caught(e, "写文件发生异常，校验文件格式");
+        } finally {
+            IOUtils.closeQuietly(writer);
         }
     }
 
@@ -166,12 +272,10 @@ public final class FileUtil {
         try {
             writer = new FileWriter(file, true);
             writer.append(context);
-            writer.close();
         } catch (IOException e) {
-            System.out.println(e + "\n写文件发生异常，校验文件格式");
+            ExceptionUtil.caught(e, "写文件发生异常，校验文件格式");
         } finally {
-            //            IOUtils.closeQuietly(writer);
-
+            IOUtils.closeQuietly(writer);
         }
     }
 
@@ -201,4 +305,18 @@ public final class FileUtil {
         return false;
     }
 
+    /**
+     * Check whether the director of this file exists, if not, then make this directory.
+     * 
+     * @param file the file to check
+     * @return
+     */
+    public static boolean existDirAndMakeDir(String file) {
+        File dir = (new File(file)).getParentFile();
+        if (!dir.exists()) {
+            return dir.mkdirs();
+        }
+
+        return false;
+    }
 }
