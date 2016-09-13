9427ee3aaba2bcfcf46dd59796c18525176d269a
Chao <1333776@tongji.edu.cn>
2016-09-13 10:36:45 +0800

    regular update

--- PreaBaseline/src/main/java/prea/main/MFNormalExp.java
+++ PreaBaseline/src/main/java/prea/main/MFNormalExp.java
@@ -12,7 +12,6 @@ import prea.recommender.matrix.NMF;
 import prea.recommender.matrix.PMF;
 import prea.recommender.matrix.RegularizedSVD;
 import prea.util.EvaluationMetrics;
-import prea.util.FileUtil;
 import prea.util.LoggerDefineConstant;
 import prea.util.LoggerUtil;
 import prea.util.MatrixFileUtil;
@@ -25,8 +24,8 @@ import prea.util.MatrixFileUtil;
 public class MFNormalExp {
 
     /** training dataset file output path 1m 10M100K*/
-    protected final static String[] rootDirs  = { "C:/netflix/1/", "C:/netflix/2/",
-            "C:/netflix/3/", "C:/netflix/4/", "C:/netflix/5/" };
+    protected final static String[] rootDirs  = { "C:/netflix/1/", "C:/netflix/2/", "C:/netflix/3/",
+                                                  "C:/netflix/4/", "C:/netflix/5/" };
     /** the number of rows      6040    69878 480189*/
     public final static int         userCount = 480189;
     /** the number of cloumns   3706    10677 17770*/
@@ -34,9 +33,7 @@ public class MFNormalExp {
     public final static double      maxValue  = 5.0;
     public final static double      minValue  = 1.0;
     protected final static Logger   logger    = Logger
-                                                  .getLogger(LoggerDefineConstant.SERVICE_NORMAL);
-
-    public final static String      resultDir = "C:/netflix/";
+        .getLogger(LoggerDefineConstant.SERVICE_NORMAL);
 
     /**
      * 
@@ -77,13 +74,12 @@ public class MFNormalExp {
 
         EvaluationMetrics metric = baseline.evaluate(testMatrix);
         System.out.println(metric.printMultiLine());
+        //        LoggerUtil.info(logger,
+        //            "Train: " + train + "\tTest: " + test + "\n" + metric.printMultiLine());
         LoggerUtil.info(logger,
-            "Train: " + train + "\tTest: " + test + "\n" + metric.printMultiLine());
-
-        FileUtil.writeAsAppend(
-            resultDir + "zNMF",
-            "fc: " + featureCount + "\tlr: " + lrate + "\tr: " + regularized + "\n"
-                    + metric.printOneLine() + "\n");
+            "Train: " + train + "\tTest: " + test + "\n" + String.format("%.4f", metric.getRMSE())
+                                + "\t" + String.format("%.4f", metric.getNDCG2()) + "\t"
+                                + String.format("%.4f", metric.getAveragePrecision()));
 
     }
 
@@ -94,8 +90,8 @@ public class MFNormalExp {
         SparseMatrix rateMatrix = MatrixFileUtil.read(train, userCount + 1, itemCount + 1);
 
         System.out.println("2. excute PMF. rank: " + featureCount + "\t" + new Date());
-        PMF baseline = new PMF(userCount, itemCount, maxValue, minValue, featureCount, 50, 0.4,
-            0.8, maxIter, true);
+        PMF baseline = new PMF(userCount, itemCount, maxValue, minValue, featureCount, 50, 0.4, 0.8,
+            maxIter, true);
         baseline.buildModel(rateMatrix);
         rateMatrix.clear();
         System.gc();
@@ -105,11 +101,12 @@ public class MFNormalExp {
 
         EvaluationMetrics metric = baseline.evaluate(testMatrix);
         System.out.println(metric.printMultiLine());
+        //        LoggerUtil.info(logger,
+        //            "Train: " + train + "\tTest: " + test + "\n" + metric.printMultiLine());
         LoggerUtil.info(logger,
-            "Train: " + train + "\tTest: " + test + "\n" + metric.printMultiLine());
-
-        FileUtil.writeAsAppend(resultDir + "zPMF",
-            "fc: " + featureCount + "\n" + metric.printOneLine() + "\n");
+            "Train: " + train + "\tTest: " + test + "\n" + String.format("%.4f", metric.getRMSE())
+                                + "\t" + String.format("%.4f", metric.getNDCG2()) + "\t"
+                                + String.format("%.4f", metric.getAveragePrecision()));
     }
 
     public static void doBPMF(String train, String test, int featureCount) {
@@ -129,12 +126,12 @@ public class MFNormalExp {
         SparseMatrix testMatrix = MatrixFileUtil.read(test, userCount + 1, itemCount + 1);
 
         EvaluationMetrics metric = baseline.evaluate(testMatrix);
-        System.out.println(metric.printMultiLine());
+        //        LoggerUtil.info(logger,
+        //            "Train: " + train + "\tTest: " + test + "\n" + metric.printMultiLine());
         LoggerUtil.info(logger,
-            "Train: " + train + "\tTest: " + test + "\n" + metric.printMultiLine());
-
-        FileUtil.writeAsAppend(resultDir + "zBPMF",
-            "fc: " + featureCount + "\n" + metric.printOneLine() + "\n");
+            "Train: " + train + "\tTest: " + test + "\n" + String.format("%.4f", metric.getRMSE())
+                                + "\t" + String.format("%.4f", metric.getNDCG2()) + "\t"
+                                + String.format("%.4f", metric.getAveragePrecision()));
     }
 
     public static void doRSVD(String train, String test, int featureCount) {
@@ -156,14 +153,13 @@ public class MFNormalExp {
         SparseMatrix testMatrix = MatrixFileUtil.read(test, userCount + 1, itemCount + 1);
 
         EvaluationMetrics metric = baseline.evaluate(testMatrix);
-        System.out.println(metric.printMultiLine());
+        //        System.out.println(metric.printMultiLine());
+        //        LoggerUtil.info(logger,
+        //            "Train: " + train + "\tTest: " + test + "\n" + metric.printMultiLine());
         LoggerUtil.info(logger,
-            "Train: " + train + "\tTest: " + test + "\n" + metric.printMultiLine());
-
-        FileUtil.writeAsAppend(
-            resultDir + "zSVD",
-            "fc: " + featureCount + "\tlr: " + lrate + "\tr: " + regularized + "\n"
-                    + metric.printOneLine() + "\n");
+            "Train: " + train + "\tTest: " + test + "\n" + String.format("%.4f", metric.getRMSE())
+                                + "\t" + String.format("%.4f", metric.getNDCG2()) + "\t"
+                                + String.format("%.4f", metric.getAveragePrecision()));
     }
 
 }
